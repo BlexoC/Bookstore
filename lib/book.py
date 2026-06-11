ce3da669cdc0a4f192060822cbae6fb93e8e0f2d
BlexoC <makaublessing929@gmail.com>
2026-06-11 18:04:41 +0300

    Implement Book and Coffee classes with initialization and input validation

--- lib/book.py
+++ lib/book.py
@@ -1,6 +1,29 @@
-#!/usr/bin/env python3
 
 class Book:
-    pass
+    def __init__(self,title : str, page_count):
+        self.title = title
+        self.page_count = page_count
+
+        if self.title == "":
+            print ("Enter Title")
+        else:
+            title = str(input("Enter Title"))
+        
+        if self.page_count == int:
+            page_count = int(input("Ensure it is an integer"))
+            return self.page_count
+        else:
+            print("page_count must be an integer")
+        
+    def turn_page(self):
+        if self.page_count == 0:
+            print("You Need to study") 
+        self.page_count += 1
+        print("Flipping the page...wow, you read fast!")
+
+
+        
+
+    
     
         
--- lib/coffee.py
+++ lib/coffee.py
@@ -1,4 +1,17 @@
 #!/usr/bin/env python3
 
 class Coffee:
-    pass
+    def __init__(self, size , price):
+        self.size = size
+        self.price = price
+
+    size = input("Enter the Size You Want")
+    price = int(input("Enter the Price Required"))
+
+    if  not size ["small","large","medium"]:
+        print("size must be Small, Medium, or Large")
+
+    def tip(self):
+         print ("This coffee is great, here's a tip")
+         self.price+=1
+        
